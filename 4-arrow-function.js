3aab4ffee86231b6bb9863cb57d4b95015daae07
meekailkhan <meekailkhan123aar@gmail.com>
2024-04-15 12:47:34 +0530

    this repo about how javascript work under the hood

---
+++ 4-arrow-function.js
@@ -0,0 +1,9 @@
+// arrow function 
+
+function multiplyByTwo1(input){ return input*2 };
+
+const multiplyByTwo2 = (input) => { input*2 };
+
+const multiplyByTwo3 = (input) => input*2;
+
+const multiplyByTwo4 = input => input*2
