f2a5aefea8c719f5bd2c5ca0c3b1e3c3b73bfb8d
meekailkhan <meekailkhan123aar@gmail.com>
2024-04-23 23:02:29 +0530

    this repo about how javascript work under the hood

--- 8-retaining-function.js
+++ 8-retaining-function.js
@@ -1,10 +1,12 @@
 function mainFuntion(){
     let counter = 0;
-    function innerFuntion(){ counter++}
+    function innerFuntion(){ return ++counter}
     return innerFuntion;
 }
 
 let myNewFunction = mainFuntion()
 
+console.log(myNewFunction())
+console.log(myNewFunction())
 console.log(myNewFunction())
 console.log(myNewFunction())
