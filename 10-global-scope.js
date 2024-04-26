f0b1daf6c45562917ec9f7996fff6325cfca79d4
meekailkhan <meekailkhan123aar@gmail.com>
2024-04-27 00:24:20 +0530

    this repo about how javascript work under the hood

--- 10-global-scope.js
+++ 10-global-scope.js
@@ -1,11 +1,20 @@
 var a = "one";
+
+
 console.log(a) // one
+
 console.log(window.a) // one
 
+
 {
+    var b = "two"
+    let c = "three"
     console.log(a); // one
     console.log(window.a) // one
+    console.log(c)
 }
 
-
+console.log(window) //two
+console.log(window.b) //two
+console.log(window.c)
 
