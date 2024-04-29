73a249b852f074ef0e15ef8a71c92768ffc57df9
meekailkhan <meekailkhan123aar@gmail.com>
2024-04-30 00:12:01 +0530

    this repo about how javascript work under the hood

---
+++ 11-closure-ex.js
@@ -0,0 +1,16 @@
+// Challenge 1
+// Create a function createFunction that creates and returns a function.
+// When that created function is called, it should print "hello". When you think you completed createFunction, 
+// un-comment out those
+// lines in the code and run it to see if it works.
+
+function createFunction() {
+    function innerFunction(){
+      return "hello"
+    }
+    return innerFunction;
+}
+
+
+const function1 = createFunction();
+console.log(function1()); // => should console.log('hello');
