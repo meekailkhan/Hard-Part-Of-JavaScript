4528b472a1c9e7ec840b88aa544300851a5e512a
meekailkhan <meekailkhan123aar@gmail.com>
2024-06-08 18:49:27 +0530

    this repo about how javascirpt work under the hood

--- 17-object.js
+++ 17-object.js
@@ -1,7 +1,29 @@
-function multiplyByTwo(num){
-    return num * 2
+// function multiplyByTwo(num){
+//     return num * 2
+// }
+
+// multiplyByTwo.scored = multiplyByTwo(5)
+// console.log(multiplyByTwo.scored) // 10
+// console.log(multiplyByTwo.prototype) // {}
+function userCreate(name,age,score){
+    this.name = name;
+    this.age = age;
+    this.score = score;
 }
 
-multiplyByTwo.scored = multiplyByTwo(5)
-console.log(multiplyByTwo.scored)
-console.log(multiplyByTwo.prototype)
+userCreate.prototype.scoreIncreament = function(){
+    return this.score++
+}
+userCreate.prototype.ageIncreament = function(){
+    return this.age++
+}
+userCreate.prototype.addFatherName = function(fatherName){
+    return this.name = `${this.name} ${fatherName}`
+}
+
+
+let user1 = new userCreate("meekail",18,84)
+user1.scoreIncreament()
+user1.ageIncreament()
+user1.addFatherName("Aslam")
+console.log(user1)
